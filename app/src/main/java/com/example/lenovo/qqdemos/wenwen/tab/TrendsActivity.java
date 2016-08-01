a14a094edca3b10a7f794d491b84935d4aaa29dd
wenwen <975559549@qq.com>
2016-08-01 19:53:40 +0800

    2016/8/1 v2.9 界面 顾文慧

--- app/src/main/java/com/example/lenovo/qqdemos/wenwen/tab/TrendsActivity.java
+++ app/src/main/java/com/example/lenovo/qqdemos/wenwen/tab/TrendsActivity.java
@@ -14,20 +14,5 @@ public class TrendsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tab_item_trends);
 
-        findViewById(R.id.myButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(TrendsActivity.this, TestPopWindowActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        findViewById(R.id.myButton1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent1 = new Intent(TrendsActivity.this, TestPopWindow1Activity.class);
-                startActivity(intent1);
-            }
-        });
     }
 }
